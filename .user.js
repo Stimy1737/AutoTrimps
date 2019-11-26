582e5eb09a41c1e4faa7ec2e911451325702bd39
Stimy1737 <58054925+Stimy1737@users.noreply.github.com>
2019-11-25 23:58:34 -0800

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AutoTrimps-NT-Sliverz
 // @version      1.0-NT
-// @namespace    https://slivermasterz.io/AutoTrimps
-// @updateURL    https://slivermasterz.github.io/AutoTrimps/.user.js
+// @namespace    https://Stimy1737.io/AutoTrimps
+// @updateURL    https://Stimy1737.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Meowchan
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *slivermasterz.github.io/AutoTrimps*
+// @connect      *Stimy1737.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
@@ -16,7 +16,7 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-NT-SliverzPatch';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://slivermasterz.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://Stimy1737.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
